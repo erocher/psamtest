b936e844f2ba35bf9350041df01222540737ae2a
Evan Rocher <evanrocher@gmail.com>
2013-02-01 12:19:02 -0600

    routes?

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Twilio::Application.routes.draw do
 
 match "/twilio/incoming" => "twilio#incoming"
 match "/twilio/check" => "twilio#check"
-match "/twilio/choice/:clinicID" => "twilio#choice" #this will have clinicID
+match "/twilio/choice/:clinicID" => "twilio#choice" 
 
 end
