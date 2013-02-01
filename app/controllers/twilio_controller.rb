f7817cedd68cf0f4ebdd64315945340697eb07f5
Evan Rocher <evanrocher@gmail.com>
2013-01-31 19:42:29 -0600

    resetting

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,2 +1,5 @@
 class TwilioController < ApplicationController
+  def incoming
+    render :action => "greet.xml.builder"
+  end
 end
---
+++ app/views/twilio/greet.xml.builder
@@ -0,0 +1,5 @@
+xml.instruct!
+xml.Response do
+  xml.Say "Greetings."
+  xml.Hangup
+end
