4bf1dff16fdac59ce98464ecf4c8fca43187884f
Evan Rocher <evanrocher@gmail.com>
2013-02-01 12:15:09 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Twilio::Application.routes.draw do
 
 match "/twilio/incoming" => "twilio#incoming"
 match "/twilio/check" => "twilio#check"
-math "/twilio/choice/:clinicID" => "twilio#choice" #this will have clinicID
+match "/twilio/choice/:clinicID" => "twilio#choice" #this will have clinicID
 
 end
