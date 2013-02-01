4a920413e6f98f13585e03aa7a20dd83ce858144
Evan Rocher <evanrocher@gmail.com>
2013-01-31 19:35:10 -0600

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twilio::Application.config.secret_token = '47c7e86d4ee6fd67fb705a065b080ca34001febfd6b624b1af0da586b275276447be98ccdc04f60e7b852423bb56a576d8a4b24052d98dab9c923b3e79a63850'
